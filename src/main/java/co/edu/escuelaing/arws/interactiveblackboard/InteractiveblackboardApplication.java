8758e1910ab4716f8d73034d2596ef358940a088
Parralol <110953563+Parralol@users.noreply.github.com>
2024-07-16 09:51:36 -0500

    Add files via upload

---
+++ src/main/java/co/edu/escuelaing/arws/interactiveblackboard/InteractiveblackboardApplication.java
@@ -0,0 +1,13 @@
+package co.edu.escuelaing.arws.interactiveblackboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteractiveblackboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteractiveblackboardApplication.class, args);
+	}
+
+}
